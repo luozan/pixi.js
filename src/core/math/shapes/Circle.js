c024ee76f83b8626f9cbecd2f757cbff15751844
AI&I <iluozan@126.com>
2018-05-24 10:20:26 +0800

    完成Circle类的翻译

--- src/core/math/shapes/Circle.js
+++ src/core/math/shapes/Circle.js
@@ -2,7 +2,7 @@ import Rectangle from './Rectangle';
 import { SHAPES } from '../../const';
 
 /**
- * The Circle object can be used to specify a hit area for displayObjects
+ * Circle对象常被用作显示对象的碰撞区域
  *
  * @class
  * @memberof PIXI
@@ -10,9 +10,9 @@ import { SHAPES } from '../../const';
 export default class Circle
 {
     /**
-     * @param {number} [x=0] - The X coordinate of the center of this circle
-     * @param {number} [y=0] - The Y coordinate of the center of this circle
-     * @param {number} [radius=0] - The radius of the circle
+     * @param {number} [x=0] - 圆心的X坐标
+     * @param {number} [y=0] - 圆心的Y坐标
+     * @param {number} [radius=0] - 圆的半径
      */
     constructor(x = 0, y = 0, radius = 0)
     {
@@ -35,7 +35,7 @@ export default class Circle
         this.radius = radius;
 
         /**
-         * The type of the object, mainly used to avoid `instanceof` checks
+         * 对象的类型，主要被用来避免`instanceof`检测
          *
          * @member {number}
          * @readOnly
@@ -46,9 +46,9 @@ export default class Circle
     }
 
     /**
-     * Creates a clone of this Circle instance
+     * 创建该Circle实例的克隆
      *
-     * @return {PIXI.Circle} a copy of the Circle
+     * @return {PIXI.Circle} Circle对象的克隆
      */
     clone()
     {
@@ -56,11 +56,11 @@ export default class Circle
     }
 
     /**
-     * Checks whether the x and y coordinates given are contained within this circle
+     * 检查传入的X和Y坐标是否包含在这个圆形内。
      *
-     * @param {number} x - The X coordinate of the point to test
-     * @param {number} y - The Y coordinate of the point to test
-     * @return {boolean} Whether the x/y coordinates are within this Circle
+     * @param {number} x - 测试点的X坐标
+     * @param {number} y - 测试点的Y坐标
+     * @return {boolean} x/y坐标是否在这个圆形内
      */
     contains(x, y)
     {
@@ -80,9 +80,9 @@ export default class Circle
     }
 
     /**
-    * Returns the framing rectangle of the circle as a Rectangle object
+    * 返回这个圆形的包围盒，返回结果是个Rectangle对象
     *
-    * @return {PIXI.Rectangle} the framing rectangle
+    * @return {PIXI.Rectangle} 包围盒矩形对象
     */
     getBounds()
     {
